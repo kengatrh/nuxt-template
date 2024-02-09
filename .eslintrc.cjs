8231e07bdb74a5464d76bae2f7287b518055c06f
Keng Fordham <kengfordham@Kengs-Mac-mini.lan>
2024-02-09 23:27:54 +0000

    initial template

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
+};
