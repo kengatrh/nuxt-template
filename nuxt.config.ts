0351eae794e2365b9b8610b0ce492b2c0291dc27
Keng Fordham <keng@pastimehub.com>
2024-02-11 01:31:24 +0000

    installed primevue orinal package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["nuxt-primevue"],
   typescript: {
     shim: false,
     typeCheck: true,
     strict: true,
   },
+  primevue: {
+    options: {
+      ripple: true,
+    },
+    components: {
+      include: ["Button"],
+    },
+  },
+  css: [
+    "primevue/resources/themes/lara-dark-teal/theme.css",
+    "primevue/resources/primevue.min.css",
+    "primeicons/primeicons.css",
+    "primeflex/primeflex.css",
+  ],
 });
